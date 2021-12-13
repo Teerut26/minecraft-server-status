ec6474cafdb12d32eaf6b883424c3fede8cb8b5d
Teerut26 <teerutsr@gmail.com>
2021-12-13 23:00:27 +0700

    1.0.0

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(props) {
   const [ShowAlertData, setShowAlertData] = useState("");
   const getData = async () => {
     let res = await fetch(
-      "https://api.mcsrvstat.us/2/teerut.duckdns.org"
+      "https://mcapi.us/server/status?ip=teerut.duckgdns.org"
     );
     let data = await res.json();
     setData(data);
@@ -20,54 +20,65 @@ function App(props) {
     getData();
     setInterval(() => {
       getData();
-    }, 5000);
+    }, 10000);
     
   }, []);
 
   if (!Data) {
     return <></>;
   }
+  
   return (
-    <div className="flex h-screen w-full justify-center items-center ">
-      <div className="border-2 px-3 rounded-lg shadow-md">
-        <div className="flex justify-center items-center gap-2">
-          <img src={Data.icon} />
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <div className="text-2xl font-bold">{Data.motd.html}</div>
-        </div>
-        <div className="flex justify-center items-center gap-2 mb-2">
-          <i class="fas fa-users"></i>
-          <div className="text-1xl font-bold">
-            {Data.players.online}/{Data.players.max}
-          </div>
-        </div>
-        {ShowAlert && (
-          <div className="alert alert-success" role="alert">
-            <i class="fas fa-check"></i> {ShowAlertData}
-          </div>
-        )}
-
-        <div className="flex flex-col justify-right items-center gap-2 mb-2 list overflow-auto">
-          {Data.players.list.map((item) => (
-            <Card onCoppyId={(e) => {
-              setShowAlert(true)
-              setShowAlertData(e)
-              setTimeout(() => {
-                setShowAlert(false)
-                setShowAlertData("")
-              }, 1500);
-            }} name={item} />
-          ))}
+    Data.online ? <div className="flex h-screen w-full justify-center items-center ">
+    <div className="border-2 px-3 rounded-lg shadow-md">
+      <div className="flex justify-center items-center gap-2">
+        <img src={Data.favicon} />
+      </div>
+      <div className="flex justify-center items-center gap-2">
+        <div className="text-2xl font-bold">{Data.motd}</div>
+      </div>
+      <div className="flex justify-center items-center gap-2 mb-2">
+        <i class="fas fa-users"></i>
+        <div className="text-1xl font-bold">
+          {Data.players.now}/{Data.players.max}
         </div>
       </div>
+      {ShowAlert && (
+        <div className="alert alert-success" role="alert">
+          <i class="fas fa-check"></i> {ShowAlertData}
+        </div>
+      )}
+
+      <div className="flex flex-col justify-right items-center gap-2 mb-2 list overflow-auto">
+        {Data.players.sample.map((item) => (
+          <Card onCoppyId={(e) => {
+            setShowAlert(true)
+            setShowAlertData(e)
+            setTimeout(() => {
+              setShowAlert(false)
+              setShowAlertData("")
+            }, 1500);
+          }} {...item} />
+        ))}
+      </div>
     </div>
+  </div> : <OffLine />
   );
 }
 
 export default connect((state) => {
   return state;
 })(App);
+
+const OffLine = () => {
+  return (
+    <div className="flex h-screen w-full justify-center items-center ">
+      <div className="font-bold text-5xl p-3 ">
+        OFFLINE
+      </div>
+    </div>
+  )
+}
 
 const Card = ({ name, id, onCoppyId }) => {
   const coppyId = (name) => {
